ea872182630f71320e607f645819c026a119b5ea
Vishwajeet Mishra <135427511+vishwajeetmishra4@users.noreply.github.com>
2024-06-25 11:27:20 +0530

    Day 3.java

---
+++ Day 3.java
@@ -0,0 +1,31 @@
+// Day 3 of Leetcode Challange
+
+// Problem Number 1038: Binary Search Tree to Greater Search Tree
+
+class TreeNode {
+  int val;
+  TreeNode left;
+  TreeNode right;
+  TreeNode(int x) { val = x; }
+}
+
+class Solution {
+  public TreeNode bstToGst(TreeNode root) {
+    final int[] prefix = {0};
+
+    reversedInorder(root, prefix);
+    return root;
+  }
+
+  private void reversedInorder(TreeNode root, int[] prefix) {
+    if (root == null)
+      return;
+
+    reversedInorder(root.right, prefix);
+
+    root.val += prefix[0];
+    prefix[0] = root.val;
+
+    reversedInorder(root.left, prefix);
+  }
+}
