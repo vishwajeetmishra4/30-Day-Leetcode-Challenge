9cdbd245d8fca0cccf5de9f8dc86a2e6cddbc453
Vishwajeet Mishra <135427511+vishwajeetmishra4@users.noreply.github.com>
2024-06-28 12:52:45 +0530

    Day 5.java

--- Day 5.java
+++ Day 5.java
@@ -1,3 +1,11 @@
 // Day 5 of 30 Days Leetcode.
 
-// Problem NUmber: 
+// Problem Number: 1791 Find the Centre of Star Graph.
+
+class Solution {
+    public int findCenter(int[][] edges) {
+        return (edges[0][0] == edges[1][0] || edges[0][0] == edges[1][1])
+                ? edges[0][0]
+                : edges[0][1];
+    }
+}
