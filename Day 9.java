edc487df72e741b39ee364807d9b5176fa2e7aa6
Vishwajeet Mishra <135427511+vishwajeetmishra4@users.noreply.github.com>
2024-07-01 22:33:18 +0530

    Day 9.java

---
+++ Day 9.java
@@ -0,0 +1,15 @@
+// Day 9 of 30 Days Leetcode.
+
+// Problem Number: 
+
+class Solution {
+  public boolean threeConsecutiveOdds(int[] arr) {
+    int count = 0;
+    for (final int a : arr) {
+      count = a % 2 == 1 ? count + 1 : 0;
+      if (count == 3)
+        return true;
+    }
+    return false;
+  }
+}
