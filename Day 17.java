c05abc827f7569aad4ca2f19e9928a989a00c654
Vishwajeet Mishra <135427511+vishwajeetmishra4@users.noreply.github.com>
2024-07-09 13:54:09 +0530

    Day 17.java

---
+++ Day 17.java
@@ -0,0 +1,17 @@
+// Day 17 of 30 days Leetcode.
+
+// Problem Number 1701:Average Waiting Time.
+
+class Solution {
+  public double averageWaitingTime(int[][] customers) {
+    double wait = 0;
+    double curr = 0;
+
+    for (int[] c : customers) {
+      curr = Math.max(curr, 1.0 * c[0]) + c[1];
+      wait += curr - c[0];
+    }
+
+    return 1.0 * wait / customers.length;
+  }
+}
